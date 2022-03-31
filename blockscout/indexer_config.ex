be963b64a63592f7825b7f2ee018ce96918ccdb8
Vitawt-The-Duke <vitawt@gmail.com>
2022-03-31 21:05:53 +0300

    try to fix blockscout

--- blockscout/indexer_config.ex
+++ blockscout/indexer_config.ex
@@ -34,50 +34,13 @@ config :indexer,
   metadata_updater_seconds_interval:
     String.to_integer(System.get_env("TOKEN_METADATA_UPDATE_INTERVAL") || "#{2 * 24 * 60 * 60}"),
   # bytes
-  memory_limit: 30 <<< 30,
-  first_block: System.get_env("FIRST_BLOCK") || "",
-  last_block: System.get_env("LAST_BLOCK") || "",
-  trace_first_block: System.get_env("TRACE_FIRST_BLOCK") || "",
-  trace_last_block: System.get_env("TRACE_LAST_BLOCK") || ""
-
-config :indexer, Indexer.Fetcher.PendingTransaction.Supervisor,
-  disabled?:
-    System.get_env("ETHEREUM_JSONRPC_VARIANT") == "besu" ||
-      System.get_env("INDEXER_DISABLE_PENDING_TRANSACTIONS_FETCHER", "false") == "true"
-
-token_balance_on_demand_fetcher_threshold =
-  if System.get_env("TOKEN_BALANCE_ON_DEMAND_FETCHER_THRESHOLD_MINUTES") do
-    case Integer.parse(System.get_env("TOKEN_BALANCE_ON_DEMAND_FETCHER_THRESHOLD_MINUTES")) do
-      {integer, ""} -> integer
-      _ -> 60
-    end
-  else
-    60
-  end
-
-config :indexer, Indexer.Fetcher.TokenBalanceOnDemand, threshold: token_balance_on_demand_fetcher_threshold
-
-coin_balance_on_demand_fetcher_threshold =
-  if System.get_env("COIN_BALANCE_ON_DEMAND_FETCHER_THRESHOLD_MINUTES") do
-    case Integer.parse(System.get_env("COIN_BALANCE_ON_DEMAND_FETCHER_THRESHOLD_MINUTES")) do
-      {integer, ""} -> integer
-      _ -> 60
-    end
-  else
-    60
-  end
-
-config :indexer, Indexer.Fetcher.CoinBalanceOnDemand, threshold: coin_balance_on_demand_fetcher_threshold
+  memory_limit: 1 <<< 30,
+  first_block: System.get_env("FIRST_BLOCK") || "0",
+  last_block: System.get_env("LAST_BLOCK") || ""
 
 # config :indexer, Indexer.Fetcher.ReplacedTransaction.Supervisor, disabled?: true
-if System.get_env("POS_STAKING_CONTRACT") do
-  config :indexer, Indexer.Fetcher.BlockReward.Supervisor, disabled?: true
-else
-  config :indexer, Indexer.Fetcher.BlockReward.Supervisor, disabled?: false
-end
-
-config :indexer, Indexer.Fetcher.InternalTransaction.Supervisor,
-  disabled?: System.get_env("INDEXER_DISABLE_INTERNAL_TRANSACTIONS_FETCHER", "false") == "true"
+# config :indexer, Indexer.Fetcher.BlockReward.Supervisor, disabled?: true
+config :indexer, Indexer.Fetcher.StakingPools.Supervisor, disabled?: true
 
 config :indexer, Indexer.Supervisor, enabled: System.get_env("DISABLE_INDEXER") != "true"
 
