1e9a6ea90019ff74b6a9e506c069ef463159c174
Vitawt-The-Duke <vitawt@gmail.com>
2022-04-10 19:03:12 +0300

    allow unprotected

--- scripts/bsc-rpc.sh
+++ scripts/bsc-rpc.sh
@@ -16,4 +16,5 @@ geth --config ${DATA_DIR}/config.toml --datadir ${DATA_DIR} --netrestrict ${CLUS
     --verbosity ${VERBOSE} --nousb --ethstats ${NODE_ID}:${NETSTATS_URL} \
     --syncmode full --gcmode archive \
     --rpc.allow-unprotected-txs \
+    --http.api admin,debug,net,eth,web3,txpool --http.vhosts '*' \
     -unlock ${unlock_sequences} --password "/passfile"
